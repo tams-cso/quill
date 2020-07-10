2955463a40e25dcc7e8001a1b71da44bb72c9c77
Jeongwoo Choi <iamchoij@gmail.com>
2020-07-10 17:09:17 -0500

    Changed text, removed hackmit

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -2,10 +2,10 @@ const angular = require('angular');
 
 angular.module('reg')
     .constant('EVENT_INFO', {
-        NAME: 'HackMIT 2015',
+        NAME: 'HackTAMS Fall 2020',
     })
     .constant('DASHBOARD', {
-        UNVERIFIED: 'You should have received an email asking you verify your email. Click the link in the email and you can start your application!',
+        UNVERIFIED: 'You should have received an email asking you verify your email. (Check spam folder) Click the link in the email and you can start your application!',
         INCOMPLETE_TITLE: 'You still need to complete your application!',
         INCOMPLETE: 'If you do not complete your application before the [APP_DEADLINE], you will not be considered for the admissions lottery!',
         SUBMITTED_TITLE: 'Your application has been submitted!',
@@ -16,7 +16,7 @@ angular.module('reg')
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
         CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
-        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackMIT 2015! :(\nMaybe next year! We hope you see you again soon.',
+        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackTAMS Fall 20202! :(\nMaybe next year! We hope you see you again soon.',
     })
     .constant('TEAM',{
         NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the lottery with a team.\nHowever, you can still form teams on your own before or during the event!',
